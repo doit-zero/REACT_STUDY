1f8bbac31da2a1f98410c3eab2b082e5466a3f9b
DoubleJ <131241458+doit-zero@users.noreply.github.com>
2025-03-19 23:29:05 +0900

    리액트 글쓰기 기능 추가
    - 리액트 상태 변환 이론 공부

--- src/App.js
+++ src/App.js
@@ -34,22 +34,37 @@ function Nav(props){
   </nav>
 }
 
-function Create() {
-  return <a href="/create" onClick={event => {
-    event.preventDefault();
-
-  }}>Create</a>;
+function Create(props) {
+  return <article>
+    <form onSubmit={event =>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title,body);
+    }}>
+    <h2>Create</h2>
+      <p><input type="title" name="title" placeholder="title" /></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create" /></p>
+    </form>
+  </article>
 }
 
 function App() {
+
   const [mode, setMode] = useState('WELCOME');
+
   const [id, setId] = useState(null);
-  const topics = [
+
+  const [nextId,setNextId] = useState(4);
+  const [topics,setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
-  ]
+  ]);
+
   let content = null;
+
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB"></Article>
   } else if(mode === 'READ'){
@@ -62,7 +77,19 @@ function App() {
     }
     content = <Article title={title} body={body}></Article>
   } else if(mode === 'CREATE'){
-    console.log("CREATE MODE");
+    content = <Create onCreate={(title,body)=>{
+      const newTopic = {id:nextId,title:title,body:body}
+      // 리액트는 원시 타입이 아닌 경우 복제 해야함
+      const newTopics = [...topics];
+      // 복제한 값에 새로운 토픽을 넣어서
+      newTopics.push(newTopic);
+      // 리액트는 기존의 배열과 새롭게 생성된 배열이 다름을 인지하여 렌더링이 됨
+      setTopics(newTopics);
+
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+    }}></Create>
   }
   return (
       <div>
